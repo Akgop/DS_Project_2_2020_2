6170731c133b7c6ff425bda28adf253a1fc326f1
Prof. Ki-Hoon Lee <50433145+DSLDataStorage@users.noreply.github.com>
2020-10-06 03:13:30 +0900

    Update FPGrowth.h

--- FPGrowth.h
+++ FPGrowth.h
@@ -26,10 +26,10 @@ public:
 	
 	void SubsetSetting() {
 		table->ascendingIndexTable();
-		subSet = setSubsetting(table, fpTree);
+		subSet = getSubset(table, fpTree);
 	}
 
-	map<set<string>, int> setSubsetting(HeaderTable* pTable, FPNode* pTree);
+	map<set<string>, int> getSubset(HeaderTable* pTable, FPNode* pTree);
 	void powerSet(map<set<string>, int>* subset, vector<string> data, string item, int frequency, int* flag, int depth);
 	bool contains_single_path(FPNode* pNode);
 	int item_frequency(string item) {return table->find_frequency(item);}
